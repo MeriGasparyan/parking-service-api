9f1ae3fa8acc5aaa5afb586a2749995be9f5e4d5
MeriGasparyan <merigas04@gmail.com>
2025-08-09 12:52:21 +0400

    Setup project

---
+++ src/test/java/com/merigasparyan/jmp/parkingserviceapi/ParkingServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merigasparyan.jmp.parkingserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
